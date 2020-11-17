efd28846b4a8c26906e6298588e739b1371d5bd8
sivaprasadsa <53429732+sivaprasadsa@users.noreply.github.com>
2020-11-17 09:59:07 +0530

    Add files via upload

--- dict.py
+++ dict.py
@@ -1,16 +1,12 @@
 import os
 direc= 'files/'
 file_dic= {}
-
+#import pdb;pdb.set_trace()
 list_files= [i for i in os.listdir(direc)]
-#list_time = [j for j in  os.path.getctime(direc)]
-#print(list_files)
-for key in file_dic: 
-    for value in list_time: 
-        file_dic[key] = value 
-        break 
-#print(list_time)
-for i in list_files:
-    key = list_files.index(i)+1
-    file_dic[key]=i
-print (file_dic)
+#list_time = [j for j in  os.path.getctime()]
+for j in list_files:
+    m_time = ""
+    m_time = os.path.getctime("files/"+j)
+    file_dic[j] = m_time
+
+print(file_dic)
